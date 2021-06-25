bb46590ae48f51a391ca0dda43809b69f59ea3a1
asyks <aaron@aaronsykes.com>
2021-06-24 20:27:09 -0700

    Dispatch post added action

--- src/features/posts/AddPostsForm.js
+++ src/features/posts/AddPostsForm.js
@@ -1,12 +1,29 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { nanoid } from '@reduxjs/toolkit'
+
+import { postAdded } from './postsSlice'
 
 export const AddPostsForm = () => {
 
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
 
+  const dispatch = useDispatch()
+
   const onTitleChange = e => setTitle(e.target.value)
   const onContentChange = e => setContent(e.target.value)
+
+  const onSavePostClick = () => {
+    if (title && content) {
+      dispatch(
+        postAdded({id: nanoid(), title, content})
+      )
+
+      setTitle('')
+      setContent('')
+    }
+  }
 
   return (
     <section>
@@ -28,7 +45,7 @@ export const AddPostsForm = () => {
           value={content}
           onChange={onContentChange}
         />
-        <button type="button">Save Post</button>
+        <button type="button" onClick={onSavePostClick}>Save Post</button>
       </form>
     </section>
   )
