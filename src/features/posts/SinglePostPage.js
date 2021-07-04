ee985f46f40a75717d403f6f339aa489f2856219
asyks <aaron@aaronsykes.com>
2021-07-03 19:39:00 -0700

    Display post created timestamp on post list and single post pages

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
 
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
@@ -23,6 +24,7 @@ export const SinglePostPage = ({ match }) => {
       <article>
         <h2>{post.title}</h2>
         <PostAuthor userId={post.user}/>
+        <TimeAgo timestamp={post.date}/>
         <p className="post-content">{post.content}</p>
         <Link to={`/editPost/${postId}`} className="button">
           Edit Post
