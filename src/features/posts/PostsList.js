0d2df061e57a93edc982bb464742bdbd65d5a146
asyks <aaron@aaronsykes.com>
2021-09-22 20:43:28 -0700

    Refactor post excerpt into separate component

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,14 +2,34 @@ import React, { useEffect } from 'react'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from './TimeAgo'
 import { ReactionButtons } from './ReactionButtons'
 import { fetchPosts, selectAllPosts, selectError, selectStatus } from './postsSlice'
 
+export const PostExcerpt = ({ post }) => {
+  return (
+    <article className="post-excerpt" key={post.id}>
+      <h3>{post.title}</h3>
+      <div>
+        <PostAuthor userId={post.user}/>
+        <TimeAgo timestamp={post.date}/>
+      </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+
+      <ReactionButtons post={post}/>
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+    </article>
+  )
+}
+
 export const PostsList = () => {
   const dispatch = useDispatch()
   const posts = useSelector(selectAllPosts)
+
   const postStatus = useSelector(selectStatus)
   const error = useSelector(selectError)
 
@@ -24,18 +44,12 @@ export const PostsList = () => {
   if (postStatus === 'loading') {
     content = <div className="loader">Loading...</div>
   } else if (postStatus === "succeeded") {
-    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date))
+
     content = orderedPosts.map(post => (
-      <article className="post-excerpt" key={post.id}>
-        <h3>{post.title}</h3>
-        <PostAuthor userId={post.user}/>
-        <TimeAgo timestamp={post.date}/>
-        <p className="post-content">{post.content.substring(0, 100)}</p>
-        <Link to={`/posts/${post.id}`} className="button muted-button">
-          View Post
-        </Link>
-        <ReactionButtons post={post}/>
-      </article>
+      <PostExcerpt key={post.id} post={post} />
     ))
   } else if (postStatus === "failed") {
     content = <div>{error}</div>
