8367dd6fb82c0e85348975a9ae1e8cb821d5aaeb
asyks <aaron@aaronsykes.com>
2021-09-24 20:39:00 -0700

    Normalize notifications slice state

--- src/features/notifications/notificationsSlice.js
+++ src/features/notifications/notificationsSlice.js
@@ -1,6 +1,14 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import {
+  createSlice,
+  createAsyncThunk,
+  createEntityAdapter
+} from "@reduxjs/toolkit";
 
 import { client } from '../../api/client'
+
+const notificationsAdapter = createEntityAdapter({
+  sortComparer: (a, b) => b.date.localeCompare(a.date)
+})
 
 export const fetchNotifications = createAsyncThunk(
 'notifications/fetchNotifications',
@@ -16,17 +24,20 @@ export const fetchNotifications = createAsyncThunk(
 
 const notificationsSlice = createSlice({
   name: 'notifications',
-  initialState: [],
+  initialState: notificationsAdapter.getInitialState(),
   reducers: {
     allNotificationsRead(state, action) {
-      state.forEach(notification => {notification.read = true})
+      Object.values(state.entities).forEach(notification => {
+        notification.read = true
+      })
     }
   },
   extraReducers: {
     [fetchNotifications.fulfilled]: (state, action) => {
-      state.push(...action.payload)
-      state.forEach(notification => notification.isNew = !notification.read)
-      state.sort((a, b) => b.date.localeCompare(a.date))
+      Object.values(state.entities).forEach(notification => {
+        notification.isNew = !notification.read
+      })
+      notificationsAdapter.upsertMany(state, action.payload)
     }
   }
 })
@@ -35,4 +46,6 @@ export const {allNotificationsRead} = notificationsSlice.actions
 
 export default notificationsSlice.reducer
 
-export const selectAllNotifications = state => state.notifications
+export const {
+  selectAll: selectAllNotifications
+} = notificationsAdapter.getSelectors(state => state.notifications)
